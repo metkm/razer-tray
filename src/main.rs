b86f80ddfd28e5a9efc03321d37e3f2b37c90e49
metkm <metinkorkmaz417@gmail.com>
2024-10-12 01:31:16 +0300

    update tooltip from other thread

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,51 @@
+#![windows_subsystem = "windows"]
+
 mod battery;
 
 use battery::get_battery;
 use image::GenericImageView;
-use tray_icon::TrayIconBuilder;
-use tao::event_loop::EventLoop;
+use tao::event_loop::{ControlFlow, EventLoop};
+use tray_icon::{menu::MenuEvent, TrayIconBuilder};
 
 fn main() {
-    let icon= include_bytes!("../trayicon.jpg");
+    let icon = include_bytes!("../trayicon.jpg");
 
     let img = image::load_from_memory(icon).unwrap();
     let img_raw = img.to_rgba8().into_raw();
     let (img_w, img_h) = img.dimensions();
 
+    let quit_item = tray_icon::menu::MenuItem::with_id("quit", "quit", true, None);
+    let menu = tray_icon::menu::Menu::with_items(&[&quit_item]).unwrap();
+
     let tray_icon = TrayIconBuilder::new()
         .with_tooltip("Razer Tray")
+        .with_menu(Box::new(menu))
         .with_icon(tray_icon::Icon::from_rgba(img_raw, img_w, img_h).unwrap())
         .build()
         .unwrap();
 
     let event_loop = EventLoop::new();
 
-    event_loop.run(move |_, _, _| {
-        let Ok(battery) = get_battery() else {
-            return;
-        };
+    let mut battery_level_text = String::new();
 
-        let text = format!("Batter Level {}%", battery as u8);
-        tray_icon.set_tooltip(Some(text)).ok();
+    std::thread::scope(|s| {
+        s.spawn(|| {
+            let battery = get_battery().unwrap() as u8;
+            battery_level_text = format!("Batter Level {}%", battery as u8);
+        });
+    });
 
-        std::thread::sleep(std::time::Duration::from_secs(30));
+    event_loop.run(move |_, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
+
+        if let Ok(MenuEvent { id }) = MenuEvent::receiver().try_recv() {
+            let quit_id = quit_item.id();
+
+            if id == quit_id {
+                std::process::exit(0);
+            }
+        }
+
+        tray_icon.set_tooltip(Some(&battery_level_text)).ok();
     });
 }
