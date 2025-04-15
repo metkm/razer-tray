9f07640c63419058f309be92736769de9d4f7cfc
metkm <metinkorkmaz417@gmail.com>
2025-04-16 00:38:21 +0300

    rewrite for new mouse

--- src/battery.rs
+++ src/battery.rs
@@ -1,104 +1,108 @@
-use std::{mem::offset_of, time::Duration};
+use hidapi::HidApi;
 
-#[repr(packed)]
-#[derive(Debug)]
-#[allow(dead_code)]
-struct RazerReport {
-    status: u8,
-    transaction_id: u8,
-    remaining_packets: u16,
-    protocol_type: u8,
-    data_size: u8,
-    command_class: u8,
-    command_id: u8,
-    arguments: [i8; 80],
-    crc: u8,
-    reserved: u8,
-}
+pub fn get_battery() -> Option<u8> {
+    let api = HidApi::new().unwrap();
 
-impl RazerReport {
-    fn new(transaction_id: u8, command_class: u8, command_id: u8) -> Self {
-        Self {
-            status: 0x00,
-            transaction_id,
-            remaining_packets: 0x00,
-            protocol_type: 0x00,
-            command_class,
-            command_id,
-            data_size: 0x80,
-            arguments: [0; 80],
-            crc: 0x00,
-            reserved: 0x00
-        }
-    }
-}
-
-unsafe fn struct_to_bytes<T>(p: &mut T) -> &mut [u8] {
-    ::core::slice::from_raw_parts_mut(
-        (p as *mut T) as *mut u8,
-        ::core::mem::size_of::<T>()
-    )
-}
-
-pub fn get_battery() -> rusb::Result<f32> {
-    let Some(mouse) = rusb::devices()?.iter().find(|device| {
-        let device_desc = device.device_descriptor().unwrap();
-        let vendor_hex = format!("{:x}", device_desc.vendor_id());
-
-        vendor_hex == "1532"
-    }) else {
-        return Err(rusb::Error::NoDevice);
+    let Some(device) = api
+        .device_list()
+        .skip(2)
+        .find(|device| device.vendor_id() == 13652)
+    else {
+        return None;
     };
 
-    let mut request = RazerReport::new(0x1f, 0x07, 0x80);
-    let mut request_bytes = unsafe { struct_to_bytes(&mut request) };
+    let hid_device = device.open_device(&api).unwrap();
+    println!("{:?}", hid_device);
+
+    let mut buff = vec![
+        0x08, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00,
+    ];
 
     let mut crc = 0;
-
-    for i in &request_bytes[2..] {
-        crc ^= i;
+    for i in 0..(buff.len() - 1) {
+        crc += buff[i];
     }
 
-    request.crc = crc;
-    request_bytes = unsafe { struct_to_bytes(&mut request) };
+    crc = crc & 0xff;
+    crc = 0x55 - crc;
 
-    let device_handle = mouse.open().unwrap();
-    let write_request_type = rusb::request_type(
-        rusb::Direction::Out,
-        rusb::RequestType::Class,
-        rusb::Recipient::Interface,
-    );
+    buff.push(crc);
 
-    device_handle.write_control(
-        write_request_type,
-        0x09,
-        0x300,
-        0x00,
-        &request_bytes,
-        Duration::from_secs(1),
-    )?;
+    let written_count = hid_device.write(&buff).unwrap();
 
-    std::thread::sleep(Duration::from_millis(100));
+    let mut response_buff: Vec<u8> = Vec::with_capacity(16);
+    unsafe {
+        response_buff.set_len(written_count);
+    }
 
-    let read_request_type = rusb::request_type(
-        rusb::Direction::In,
-        rusb::RequestType::Class,
-        rusb::Recipient::Interface,
-    );
+    hid_device.read(&mut response_buff).unwrap();
 
-    device_handle.read_control(
-        read_request_type,
-        0x01,
-        0x300,
-        0x00,
-        request_bytes,
-        Duration::from_secs(1),
-    )?;
+    let battery = response_buff.get(6).unwrap().to_owned();
 
-    let arguments_offset = offset_of!(RazerReport, arguments);
-    let battery_level = *request_bytes.get(arguments_offset + 1).unwrap() as f32;
-    let battery_level_normalized = (battery_level / 255.0) * 100.0;
+    Some(battery)
 
-    Ok(battery_level_normalized)
+    /////////
+    // Old razer code
+    /////////
+
+    // let Some(mouse) = rusb::devices()?.iter().find(|device| {
+    //     let device_desc = device.device_descriptor().unwrap();
+    //     let vendor_hex = format!("{:x}", device_desc.vendor_id());
+
+    //     vendor_hex == "1532"
+    // }) else {
+    //     return Err(rusb::Error::NoDevice);
+    // };
+
+    // let mut request = RazerReport::new(0x1f, 0x07, 0x80);
+    // let mut request_bytes = unsafe { struct_to_bytes(&mut request) };
+
+    // let mut crc = 0;
+
+    // for i in &request_bytes[2..] {
+    //     crc ^= i;
+    // }
+
+    // request.crc = crc;
+    // request_bytes = unsafe { struct_to_bytes(&mut request) };
+
+    // let device_handle = mouse.open().unwrap();
+    // let write_request_type = rusb::request_type(
+    //     rusb::Direction::Out,
+    //     rusb::RequestType::Class,
+    //     rusb::Recipient::Interface,
+    // );
+
+    // device_handle.write_control(
+    //     write_request_type,
+    //     0x09,
+    //     0x300,
+    //     0x00,
+    //     &request_bytes,
+    //     Duration::from_secs(1),
+    // )?;
+
+    // std::thread::sleep(Duration::from_millis(100));
+
+    // let read_request_type = rusb::request_type(
+    //     rusb::Direction::In,
+    //     rusb::RequestType::Class,
+    //     rusb::Recipient::Interface,
+    // );
+
+    // device_handle.read_control(
+    //     read_request_type,
+    //     0x01,
+    //     0x300,
+    //     0x00,
+    //     request_bytes,
+    //     Duration::from_secs(1),
+    // )?;
+
+    // let arguments_offset = offset_of!(RazerReport, arguments);
+    // let battery_level = *request_bytes.get(arguments_offset + 1).unwrap() as f32;
+    // let battery_level_normalized = (battery_level / 255.0) * 100.0;
+
+    // Ok(battery_level_normalized)
 }
-
